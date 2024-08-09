43e1c2125200f760d1fb9ce172a88af96dcdbd11
HryhorievOA <ftxprox@gmail.com>
2024-08-09 17:02:44 +0300

    homework4

--- js/task-3.js
+++ js/task-3.js
@@ -1,18 +1,35 @@
 'use strict';
 
-function filterArray(numbers, value) {
-  let newNumber = [];
+const profile = {
+  username: 'Jacob',
+  playTime: 300,
+  changeUsername(newName) {
+    profile.username = newName;
+  },
+  updatePlayTime(hours) {
+    profile.playTime += hours;
+  },
+  getInfo() {
+    return `${profile.username} has ${profile.playTime} active hours!`;
+  },
+};
 
-  for (let i = 0; i < numbers.length; i += 1) {
-    if (numbers[i] > value) {
-      newNumber.push(numbers[i]);
-    }
-  }
-  return newNumber;
-}
+// function changeUsername(newName) {
+//   profile.username = newName;
+// }
 
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+// function updatePlayTime(hours) {
+//   profile.playTime += hours;
+// }
+
+// function getInfo() {
+//   return `${profile.username} has ${profile.playTime} active hours!`;
+// }
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername('Marco');
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
